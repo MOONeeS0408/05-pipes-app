ca83cd686cd21415cadff3a75ba3d2bc2d0cc69a
Jessica Ortiz <moonee0408@gmail.com>
2026-06-08 07:56:50 -0600

    feat: add Date Pipe with interval updates

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,13 +1,26 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
 })
 export default class BasicPage {
   nameLower = signal('FERNANDO');
   nameUpper = signal('fernando');
   fullName = signal('feRnAnDo heRReRa ');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+  });
 }
