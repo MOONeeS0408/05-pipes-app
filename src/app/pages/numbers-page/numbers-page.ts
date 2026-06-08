f1cc04ee4de5a094c63e9eb7d9ed67a7e69877ac
Jessica Ortiz <moonee0408@gmail.com>
2026-06-08 07:37:47 -0600

    feat: implementing Decimal, Percent and Currency pipes

--- src/app/pages/numbers-page/numbers-page.ts
+++ src/app/pages/numbers-page/numbers-page.ts
@@ -1,9 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class NumbersPage {}
+export default class NumbersPage {
+  totalSells = signal(1_246_524.5546);
+  percent = signal(0.6435);
+}
